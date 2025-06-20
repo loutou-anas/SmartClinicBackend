b7448bf5b160f88e8332dd6dc544a07682e96fce
Anas Loutou <anasloutou20@gmail.com>
2025-06-20 17:15:25 +0100

    Update PrescriptionController.java

--- src/main/java/com/smartclinic/controller/PrescriptionController.java
+++ src/main/java/com/smartclinic/controller/PrescriptionController.java
@@ -1,1 +1,21 @@
-// Placeholder content for PrescriptionController.java
+package com.smartclinic.controller;
+
+import com.smartclinic.entity.Prescription;
+import com.smartclinic.repository.PrescriptionRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+    private final PrescriptionRepository prescriptionRepository;
+
+    public PrescriptionController(PrescriptionRepository prescriptionRepository) {
+        this.prescriptionRepository = prescriptionRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Prescription> savePrescription(@RequestBody Prescription prescription) {
+        return ResponseEntity.ok(prescriptionRepository.save(prescription));
+    }
+}
